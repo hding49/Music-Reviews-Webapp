dc59374397e5c4f99fbb6febb2e69d246d9dc103
hding49 <hding49@uwo.ca>
2020-08-11 23:53:43 -0400

    update config

--- config/config.js
+++ config/config.js
@@ -4,4 +4,4 @@ var env = process.env.NODE_ENV || 'development';
 var config = require('./config.json');
 var envConfig = config[env];
 // add env. config values to process.env
-// Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
+Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
